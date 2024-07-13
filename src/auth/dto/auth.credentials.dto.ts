495fbaf2d78e26ea2be95682f7f1f3292eec9f72
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-13 21:45:23 +0200

    003 Feature_ Signing Up

---
+++ src/auth/dto/auth.credentials.dto.ts
@@ -0,0 +1,4 @@
+export class AuthCredetialsDto {
+  password: string;
+  username: string;
+}
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Repository, DataSource } from 'typeorm';
 import { UserEntity } from './user.entity';
+import { AuthCredetialsDto } from './dto/auth.credentials.dto';
 
 @Injectable()
 export class UsersRepository extends Repository<UserEntity> {
   constructor(private dataSource: DataSource) {
     super(UserEntity, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredetialsDto: AuthCredetialsDto): Promise<void> {
+    const user = this.create({ ...authCredetialsDto });
+    await this.save(user);
   }
 }
