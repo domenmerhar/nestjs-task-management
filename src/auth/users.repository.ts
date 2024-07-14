23e427a5189398e1a62a8b37beb183bae0645d4a
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-14 09:16:49 +0200

    005 Error Handling_ Username Conflicts

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Repository, DataSource } from 'typeorm';
 import { UserEntity } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth.credentials.dto';
@@ -11,6 +15,13 @@ export class UsersRepository extends Repository<UserEntity> {
 
   async createUser(authCredetialsDto: AuthCredentialsDto): Promise<void> {
     const user = this.create({ ...authCredetialsDto });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505')
+        throw new ConflictException(`Username already exists`);
+      else throw new InternalServerErrorException();
+    }
   }
 }
