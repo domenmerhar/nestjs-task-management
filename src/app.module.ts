74f33ab04c2dbaf4883e6c89d5b5b08613212895
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-16 09:22:51 +0200

    005 TypeORM Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { LoggerModule } from 'nestjs-pino';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
     }),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        autoLoadEntities: true,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+      }),
+    }),
     LoggerModule.forRoot(),
     TasksModule,
     AuthModule,
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'taskmanagement',
+  // host: 'localhost',
+  // port: 5432,
+  // username: 'postgres',
+  // password: 'postgres',
+  // database: 'taskmanagement',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   autoLoadEntities: true,
   synchronize: true,
